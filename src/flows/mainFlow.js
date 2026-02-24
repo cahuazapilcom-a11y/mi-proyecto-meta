653c6043763c7d82cdb626e6fb486f4a9a0ecaed
cahuazapilcom-a11y <cahuazapilcom@gmail.com>
2026-02-24 17:33:36 -0500

    Update greeting message for real estate service

--- src/flows/mainFlow.js
+++ src/flows/mainFlow.js
@@ -8,7 +8,7 @@ const determinarFlujo = async (numero, mensajeRecibido) => {
         .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, ""); // Esto quita puntos, comas, etc.
 
     const respuestas = {
-        "hola": "¡Hola! Bienvenido a nuestro servicio. ¿En qué puedo ayudarte?\n1. Horarios\n2. Ubicación\n3. Hablar con un asesor",
+        "hola": "¡Hola! Bienvenido a nuestro servicio de inmobiliaria ,. ¿En qué puedo ayudarte?\n1. Horarios\n2. Ubicación\n3. Hablar con un asesor",
         "horarios": "Estamos abiertos de Lunes a Viernes de 9:00 AM a 6:00 PM. 🕒",
         "ubicacion": "Nos encontramos en la Av. Principal 123, Lima. 📍",
         "gracias": "¡De nada! Es un placer ayudarte. 😊"
