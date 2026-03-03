c74797c1532004751dd8af387621562218f5cc5f
moises cahuaza <cahuazapilcom@gmail.com>
2026-03-03 06:10:50 -0500

    Corrigiendo export metaService

--- src/services/metaService.js
+++ src/services/metaService.js
@@ -13,9 +13,6 @@ const PHONE_NUMBER_ID = process.env.PHONE_NUMBER_ID;
 // ============================
 // ENVIAR MENSAJE
 // ============================
-console.log("Token usado:", process.env.META_TOKEN);
-console.log("PhoneNumberId usado:", process.env.PHONE_NUMBER_ID);
-
 
 async function sendTextMessage(to, message) {
   try {
