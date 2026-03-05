790fcf4397c9e5bba9fcaf952b51800ff47516ba
moises cahuaza <cahuazapilcom@gmail.com>
2026-03-05 14:37:51 -0500

    fix gemini model

--- src/services/geminiAiService.js
+++ src/services/geminiAiService.js
@@ -2,42 +2,29 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-// Crear modelo UNA SOLA VEZ (mejor rendimiento)
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash", // o "gemini-1.5-pro" para una versión más avanzada
+  model: "gemini-1.5-flash-latest",
   systemInstruction: `
 Eres un asesor virtual de la empresa CORPORACIÓN FLYHOUSE SAC.
-
-Responde de forma corta, clara y profesional.
-
-Reglas:
-- No saludar
-- No generar conversación larga
-- Responder solo la pregunta
-- Máximo 3 líneas
-`,
+Responde corto, claro y profesional.
+Máximo 3 líneas.
+`
 });
 
 async function geminiAiService(message) {
   try {
 
-    if (!message) {
-      return "No se recibió ninguna consulta.";
-    }
-
     const result = await model.generateContent(message);
 
     const response = await result.response;
 
-    const text = response.text();
-
-    return text;
+    return response.text();
 
   } catch (error) {
 
     console.error("❌ Error Gemini:", error.message);
 
-    return "El sistema está ocupado. Intenta nuevamente en unos minutos.";
+    return "El sistema está ocupado. Intenta nuevamente.";
 
   }
 }
