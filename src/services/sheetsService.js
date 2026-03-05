69829310d8a68177abbf65adf0bf7af9f84035f4
moises cahuaza <cahuazapilcom@gmail.com>
2026-03-05 12:15:28 -0500

    instalando gemini ai

--- src/services/sheetsService.js
+++ src/services/sheetsService.js
@@ -3,10 +3,13 @@ const { google } = require("googleapis");
 async function guardarCita(data) {
   try {
 
-    const auth = new google.auth.GoogleAuth({
-      credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS),
-      scopes: ["https://www.googleapis.com/auth/spreadsheets"]
-    });
+const auth = new google.auth.GoogleAuth({
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n")
+  },
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"]
+});
 
     const sheets = google.sheets({ version: "v4", auth });
 
