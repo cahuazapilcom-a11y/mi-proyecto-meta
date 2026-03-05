1cbd1c894519081e4887f33b00c37747d0ebf2a7
moises cahuaza <cahuazapilcom@gmail.com>
2026-03-05 13:02:35 -0500

    Actualizando servicio Gemini

--- src/services/geminiAiService.js
+++ src/services/geminiAiService.js
@@ -18,7 +18,7 @@ async function geminiAiService(message) {
   try {
 
     const model = genAI.getGenerativeModel({
-      model: "gemini-1.5-flash",
+      model: "gemini-1.0-pro",
       systemInstruction: prompt
     });
 
