6a95063c43e4aa66ac8cfe041bcdbd3f7149de83
moises cahuaza <cahuazapilcom@gmail.com>
2026-03-05 19:05:14 -0500

    fix gemini

--- src/services/geminiAiService.js
+++ src/services/geminiAiService.js
@@ -7,7 +7,7 @@ if (!process.env.GEMINI_API_KEY) {
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash-latest",
+  model: "gemini-2.5-flash",
   generationConfig: {
     temperature: 0.7,
     maxOutputTokens: 150
